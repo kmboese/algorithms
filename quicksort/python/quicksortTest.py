deb79e5d54dee25dd48d1834d0e17cafafd3b09b
Kevin Boese <kevinboese5@gmail.com>
2019-03-13 16:40:10 -0700

    Added testing for already sorted arrays

--- quicksort/python/quicksortTest.py
+++ quicksort/python/quicksortTest.py
@@ -1,10 +1,45 @@
 from quicksort import quicksort
+from random import randint
+import sys
+
+ELEMENTS = 3000
+
+# Increase max recursion limit
+sys.setrecursionlimit(ELEMENTS*2)
+
+def fillArrayRandom(arr, elements, low, high):
+    for _ in range(elements):
+        arr.append(randint(low, high))
+
+def isSorted(arr):
+    for i in range(len(arr)-1):
+        if arr[i] > arr[i+1]:
+            return False
+    return True
+
+def testRandom():
+    print("Beginning testRandom:")
+    arr = []
+    low = 0
+    high = 1000
+    fillArrayRandom(arr, ELEMENTS, low, high)
+    quicksort(arr)
+    if not isSorted(arr):
+        print(arr)
+    assert(isSorted(arr))
+    print("*****\tPASS\t*****")
+
+def testInOrder():
+    print("Beginning testInOrder:")
+    arr = [i for i in range(ELEMENTS)]
+    quicksort(arr)
+    assert(isSorted(arr))
+    print("*****\tPASS\t*****")
+
 
 def main():
-    arr = [10,80,30,90,40,50,70]
-    print("Original array: {}".format(arr))
-    quicksort(arr)
-    print("Sorted array: {}".format(arr))
+    testRandom()
+    testInOrder()
 
 if __name__ == "__main__":
     main()
