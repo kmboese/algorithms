56a768ba795a5f49b6a09a79f45d0b2c0fbc668e
Kevin Boese <kevinboese5@gmail.com>
2019-03-14 12:05:40 -0700

    Used quicksort module to sort array

--- binary-search/python/binarySearchTest.py
+++ binary-search/python/binarySearchTest.py
@@ -1,15 +1,69 @@
+import sys
+from os import path
+from random import randint
+
 from binarySearch import binarySearch
 
-def main():
-    arr = [1, 3, 5, 7, 8, 10, 15]
-    key = 20
-    index = binarySearch(arr, key)
+# Add algorithms directory to path
+sys.path.insert(0, \
+    r"D:\\github-projects\\algorithms\\quicksort\\python")
+import quicksort
+sys.path.pop(0)
 
-    if (index == -1):
-        print("Key not found")
-    else:
-        print("Key found at index {}".format(index))
+
+
+ELEMENTS = 10
+
+def testEmpty():
+    print("Running testEmpty():")
+    arr = []
+    key = 1
+    assert(binarySearch(arr, key) == -1)
+    print("***** PASS *****")
+
+def testInOrder():
+    arr = []
+    print("\nRunning testInOrder:")
+    # Insert elements in order
+    for i in range(ELEMENTS):
+        arr.append(i)
+    # Ensure we can find every element
+    for i in range(ELEMENTS):
+        assert(binarySearch(arr, i) != -1)
     
+    print("***** PASS *****")
+
+def testRandom():
+    arr = []
+    # Range for random integers
+    low, high = 0, 1000
+    print("\nRunning testRandom:")
+
+    # Insert random elements
+    for _ in range(ELEMENTS):
+        arr.append(randint(low, high))
+
+    # Sort array
+    quicksort.quicksort(arr)
+
+    # Ensure we can find all the elements in the array
+    for num in arr:
+        if (binarySearch(arr, num) == -1):
+            print("Error: number {} not found in array".format(\
+                num))
+        #assert(binarySearch(arr, num) != -1)
+    print("***** PASS *****")
+
+
+def main():
+    testEmpty()
+    testInOrder()
+    testRandom()
 
 if __name__ == "__main__":
     main()
+
+def template():
+    print("\nRunning template:")
+
+    print("***** PASS *****")
